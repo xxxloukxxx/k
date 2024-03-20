65fd132add272baeb1c59b655256465daec85841
Cedric <cedric.todaro@gmail.com>
2024-03-21 00:32:58 +0100

    ""

--- split46/firmware/config.h
+++ split46/firmware/config.h
@@ -4,9 +4,9 @@
 #define SERIAL_USART_TX_PIN GP14
 #define SERIAL_USART_RX_PIN GP8
 
-#define EE_HANDS
-#define SPLIT_HAND_PIN GP2
-#define SPLIT_HAND_PIN_LOW_IS_LEFT
+//#define EE_HANDS
+//#define SPLIT_HAND_PIN GP2
+//#define SPLIT_HAND_PIN_LOW_IS_LEFT
 
 /* #define SPLIT_HAND_PIN GP2 */
 /* #define SPLIT_HAND_PIN_LOW_IS_LEFT */
