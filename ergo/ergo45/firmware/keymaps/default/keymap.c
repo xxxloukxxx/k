264b1c980c8ef3717c0f42c9cd9160384947a6b5
Cedric <cedric.todaro@gmail.com>
2024-06-02 00:33:45 +0200

    dim. 02 juin 2024 00:18:32 CEST

--- ergo/ergo45/firmware/keymaps/default/keymap.c
+++ ergo/ergo45/firmware/keymaps/default/keymap.c
@@ -18,17 +18,18 @@
 // Layers
 enum
 {
-    QWERTY = 0,
-    NUM,
-    FUNC,
-    NUMPAD
+  QWERTY = 0,
+  NUM,
+  FUNC,
+  NAV,
+  NUMPAD
 };
 
 // Tap Dance Declarations
 enum
 {
-    TD_GUI = 0,
-    TD_TERM
+  TD_GUI = 0,
+  TD_TERM
 };
 
 tap_dance_action_t tap_dance_actions[] = {
@@ -36,93 +37,92 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 // Layout
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [QWERTY] = LAYOUT_mrtod(
-        KC_ESC, KC_ALPH_R1, KC_BSPC,
-        KC_TAB, KC_ALPH_R2, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_ALPH_R3, KC_COMM, KC_DOT, KC_UP, KC_SLSH,
-        KC_LCTL, MO(FUNC), KC_LALT, TD(TD_GUI), KC_SPACE, MO(NUM), KC_LEFT, KC_DOWN, KC_RIGHT),
+    [QWERTY] = LAYOUT_mrtod(                                                                             //
+        KC_ESC, KC_ALPH_R1, KC_BSPC,                                                                     //
+        KC_TAB, KC_ALPH_R2, KC_QUOT, KC_ENT,                                                             //
+        KC_LSFT, KC_ALPH_R3, KC_COMM, KC_DOT, KC_UP, KC_SLSH,                                            //
+        KC_LCTL, MO(FUNC), KC_LALT, TD(TD_GUI), LT(NAV, KC_SPACE), MO(NUM), KC_LEFT, KC_DOWN, KC_RIGHT), //
 
-    [NUM] = LAYOUT_mrtod(
-        KC_GRV, KC_NUMBERS, KC_DEL,
-        KC_CAPS, ___, ___, ___, ___, ___, ___, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
-        ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, KC_PGUP, KC_SCLN,
-        ___, ___, ___, ___, ___, ___, KC_HOME, KC_PGDN, KC_END),
+    [NUM] = LAYOUT_mrtod(                                                                          //
+        KC_GRV, KC_NUMBERS, KC_DEL,                                                                //
+        KC_CAPS, ___, ___, ___, ___, ___, /*   */ ___, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, //
+        ___, ___, ___, ___, ___, ___, /*   */ ___, ___, ___, ___, KC_PGUP, KC_SCLN,                //
+        ___, ___, ___, ___, /*   */___, /*   */___, KC_HOME, KC_PGDN, KC_END),                                   //
 
-    [FUNC] = LAYOUT_mrtod(
-        ___, KC_FNxx, KC_INS,
-        ___, KC_MEDIA, ___, ___, ___, KC_F11, KC_F12, ___,
-        ___, KC_PSCR, KC_SCRL, KC_PAUS, ___, ___, ___, ___, ___, ___, ___, TO(NUMPAD),
-        ___, ___, ___, ___, ___, ___, ___, ___, ___),
+    [FUNC] = LAYOUT_mrtod(                                                                     //
+        ___, KC_FNxx, KC_INS,                                                                  //
+        ___, KC_MEDIA, /*   */ ___, ___, ___, KC_F11, KC_F12, ___,                             //
+        ___, KC_PSCR, KC_SCRL, KC_PAUS, ___, ___, /*   */ ___, ___, ___, ___, ___, TO(NUMPAD), //
+        ___, ___, ___, ___, /*   */ ___, /*   */ ___, ___, ___, ___),                          //
 
-    [NUMPAD] = LAYOUT_mrtod(
-        TO(QWERTY), KC_7, KC_8, KC_9, KC_PSLS, KC_PAST, ___, ___, ___, ___, ___, ___,
-        ___, KC_4, KC_5, KC_6, KC_PMNS, KC_PPLS, ___, ___, ___, ___, ___, ___,
-        ___, KC_1, KC_2, KC_3, KC_DOT, KC_NO, ___, ___, ___, ___, ___, ___,
-        ___, ___, KC_0, KC_ENT, ___, ___, ___, ___, ___)
+    [NAV] = LAYOUT_mrtod(                                                                //
+        ___, ___, ___, ___, ___, ___, /*   */ ___, ___, KC_UP, ___, ___, ___,            //
+        ___, ___, ___, ___, ___, ___, /*   */ ___, KC_LEFT, KC_DOWN, KC_RIGHT, ___, ___, //
+        ___, ___, ___, ___, ___, ___, /*   */ ___, ___, ___, ___, ___, ___,              //
+        ___, ___, ___, ___, ___, ___, /*   */ ___, ___, ___),                            //
 
+    [NUMPAD] = LAYOUT_mrtod(                                                          //
+        TO(QWERTY), KC_7, KC_8, KC_9, KC_PSLS, KC_PAST, ___, ___, ___, ___, ___, ___, //
+        ___, KC_4, KC_5, KC_6, KC_PMNS, KC_PPLS, ___, ___, ___, ___, ___, ___,        //
+        ___, KC_1, KC_2, KC_3, KC_DOT, KC_NO, ___, ___, ___, ___, ___, ___,           //
+        ___, ___, KC_0, KC_ENT, ___, ___, ___, ___, ___)                              //
 };
 
-const uint16_t PROGMEM test_combo1[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM test_combo2[] = {KC_F, KC_G, COMBO_END};
-const uint16_t PROGMEM test_combo3[] = {KC_H, KC_J, COMBO_END};
-const uint16_t PROGMEM test_combo4[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM test_LACC[] = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM test_LBRC[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM test_LPRN[] = {KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM test_RPRN[] = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM test_RBRC[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM test_RACC[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM combo_TERM[] = {KC_ESC, KC_Q, KC_W, COMBO_END};
-//const uint16_t PROGMEM combo_sym1[] = {KC_Q, KC_A, COMBO_END};
-//const uint16_t PROGMEM combo_sym2[] = {KC_W, KC_S, COMBO_END};
-//const uint16_t PROGMEM combo_sym3[] = {KC_E, KC_D, COMBO_END};
-//const uint16_t PROGMEM combo_sym4[] = {KC_R, KC_F, COMBO_END};
-//const uint16_t PROGMEM combo_sym5[] = {KC_T, KC_G, COMBO_END};
-//const uint16_t PROGMEM combo_sym6[] = {KC_Y, KC_H, COMBO_END};
-//const uint16_t PROGMEM combo_sym7[] = {KC_U, KC_J, COMBO_END};
-//const uint16_t PROGMEM combo_sym8[] = {KC_I, KC_K, COMBO_END};
-//const uint16_t PROGMEM combo_sym9[] = {KC_O, KC_L, COMBO_END};
-//const uint16_t PROGMEM combo_sym0[] = {KC_P, KC_QUOT, COMBO_END};
+const uint16_t PROGMEM combo_HOME[] = {KC_LEFT, KC_DOWN, COMBO_END};
+const uint16_t PROGMEM combo_END[] = {KC_DOWN, KC_RIGHT, COMBO_END};
+//
+const uint16_t PROGMEM combo_BS[] = {KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM combo_ESC[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_TAB[] = {KC_A, KC_S, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(test_combo1, KC_LBRC),
-    COMBO(test_combo2, KC_LPRN),
-    COMBO(test_combo3, KC_RPRN),
-    COMBO(test_combo4, KC_RBRC),
+    COMBO(test_LACC, LSFT(KC_LBRC)),
+    COMBO(test_LBRC, KC_LBRC),
+    COMBO(test_LPRN, KC_LPRN),
+    COMBO(test_RPRN, KC_RPRN),
+    COMBO(test_RBRC, KC_RBRC),
+    COMBO(test_RACC, LSFT(KC_RBRC)),
     COMBO(combo_TERM, LGUI(LSFT(KC_ENTER))),
-//    COMBO(combo_sym1, LSFT(KC_1)),
-//    COMBO(combo_sym2, LSFT(KC_2)),
-//    COMBO(combo_sym3, LSFT(KC_3)),
-//    COMBO(combo_sym4, LSFT(KC_4)),
-//    COMBO(combo_sym5, LSFT(KC_5)),
-//    COMBO(combo_sym6, LSFT(KC_6)),
-//    COMBO(combo_sym7, LSFT(KC_7)),
-//    COMBO(combo_sym8, LSFT(KC_8)),
-//    COMBO(combo_sym9, LSFT(KC_9)),
-//    COMBO(combo_sym0, LSFT(KC_0)),
+    COMBO(combo_HOME, KC_HOME),
+    COMBO(combo_END, KC_END),
+    COMBO(combo_BS, KC_BACKSPACE),
+    COMBO(combo_ESC, KC_ESC),
+    COMBO(combo_TAB, KC_TAB),
 };
 
 void leader_start_user(void)
 {
-    // Do something when the leader key is pressed
+  // Do something when the leader key is pressed
 }
 
 void leader_end_user(void)
 {
-    if (leader_sequence_one_key(KC_ESC))
-    {
-        tap_code16(KC_MUTE);
-    }
-    else if (leader_sequence_one_key(KC_S))
-    {
-        SEND_STRING(SS_LCTL("s"));
-    }
-    else if (leader_sequence_one_key(KC_Q))
-    {
-        SEND_STRING(SS_LCTL("q"));
-    }
-    else if (leader_sequence_one_key(KC_W))
-    {
-        SEND_STRING(SS_LCTL("w"));
-    }
-    else if (leader_sequence_one_key(KC_D))
-    {
-        SEND_STRING(SS_LCTL("d"));
-    }
+  if (leader_sequence_one_key(KC_ESC))
+  {
+    tap_code16(KC_MUTE);
+  }
+  else if (leader_sequence_one_key(KC_S))
+  {
+    SEND_STRING(SS_LCTL("s"));
+  }
+  else if (leader_sequence_one_key(KC_Q))
+  {
+    SEND_STRING(SS_LCTL("q"));
+  }
+  else if (leader_sequence_one_key(KC_W))
+  {
+    SEND_STRING(SS_LCTL("w"));
+  }
+  else if (leader_sequence_one_key(KC_D))
+  {
+    SEND_STRING(SS_LCTL("d"));
+  }
 }
