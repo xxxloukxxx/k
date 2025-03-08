abb523b45a8fd3346bc2063e176678d488ec9b9f
Cedric <cedric.todaro@gmail.com>
2025-03-08 16:55:18 +0100

    sam. 08 mars 2025 16:55:18 CET

---
+++ ada38/firmware/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
+#define FORCE_NKRO
+
+#define TAPPING_TERM 200
+
+#define COMBO_TERM 25
+
+#define ENCODER_A_PINS { GP2 }
+#define ENCODER_B_PINS { GP3 }
+#define ENCODER_MAP_KEY_DELAY 15
+#define ENCODER_DIRECTION_FLIP
+#define ENCODER_RESOLUTIONS { 4 }
