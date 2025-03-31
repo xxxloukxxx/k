3efb169f59c2c841339a160c5923b77bb23c72e1
Cedric <cedric.todaro@gmail.com>
2025-03-31 22:10:16 +0200

    lun. 31 mars 2025 22:10:16 CEST

--- ada36/firmware/keymaps/default/keymap.c
+++ ada36/firmware/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /*              */ KC_Q, KC_W, KC_E, KC_R, KC_T, /* */ KC_Y, KC_U, KC_I, KC_O, KC_P,                      //
         /*              */ KC_A, KC_S, KC_D, KC_F, KC_G, /* */ KC_H, KC_J, KC_K, KC_L, KC_QUOT,                   //
         /*  */ _S(KC_Z), _C(KC_X), _A(KC_C), KC_V, KC_B, /* */ KC_N, KC_M, _A(KC_COMMA), _C(KC_DOT), _S(KC_SLSH), //
-        /*             */ _C(KC_TAB), _G(KC_ESC), RAISE, /* */ LOWER, _A(KC_BSPC), FN,                            //
+        /*             */ _C(KC_TAB), KC_LGUI, RAISE, /* */ LOWER, _A(KC_BSPC), FN,                            //
         ___, KC_MUTE),                                                                                            //
 
     [_LOWER] = LAYOUT(                                                                                //
