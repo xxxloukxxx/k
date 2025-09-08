4f55b8102b99d222ec3e7ff49595fd1257e00e16
Cédric TODARO <Cedric.todaro@ac-lyon.fr>
2025-09-08 13:22:51 +0200

    lun. 08 sept. 2025 13:16:26 CEST

--- ada36/firmware/keymaps/default/keymap.c
+++ ada36/firmware/keymaps/default/keymap.c
@@ -18,6 +18,19 @@ enum layers { _BASE, _LOWER, _RAISE, _FN, _BOTH };
 #define KC_RGT KC_RIGHT
 #define KC_DN KC_DOWN
 
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+#define HOME_QUOT RGUI_T(KC_QUOT)
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
