2bca5149c31d62cd534d751eb5b211f55a9c82b8
Cedric <cedric.todaro@gmail.com>
2025-10-29 15:25:05 +0100

    mer. 29 oct. 2025 15:25:05 CET

--- madeline36/firmware/keymaps/default/keymap.c
+++ madeline36/firmware/keymaps/default/keymap.c
@@ -1,10 +1,6 @@
 #include "quantum.h"
 
-enum layers { _BASE,
-    _LOWER,
-    _RAISE,
-    _FN,
-    _BOTH };
+enum layers { _BASE, _LOWER, _RAISE, _FN, _BOTH };
 
 /* #define HOME_ROW 1 */
 
@@ -24,75 +20,52 @@ enum layers { _BASE,
 #define KC_RGT KC_RIGHT
 #define KC_DN KC_DOWN
 
-#ifdef HOME_ROW
-// Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LSFT_T(KC_D)
-#define HOME_F LCTL_T(KC_F)
-
-// Right-hand home row mods
-#define HOME_J RCTL_T(KC_J)
-#define HOME_K RSFT_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_QUOT RGUI_T(KC_QUOT)
-#endif
-
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(                                                                      //
-        KC_Q, KC_W, KC_E, KC_R, KC_T, /* */ KC_Y, KC_U, KC_I, KC_O, KC_P,                  //
-#ifdef HOME_ROW
-        HOME_A, HOME_S, HOME_D, HOME_F, KC_G, /* */ KC_H, HOME_J, HOME_K, HOME_L, HOME_QUOT, //
-#else
-        KC_A, KC_S, KC_D, KC_F, KC_G, /* */ KC_H, KC_J, KC_K, KC_L, KC_QUOT, //
-#endif
-        _S(KC_Z), KC_X, KC_C, KC_V, KC_B, /* */ KC_N, KC_M, KC_COMMA, KC_DOT, _S(KC_SLSH), //
-        KC_LCTL, KC_LGUI, RAISE, /* */ LOWER, KC_LALT, FN                               //
-        ),                                                                                 //
+    [_BASE] = LAYOUT(                                                                         //
+        KC_Q, KC_W, KC_E, KC_R, KC_T, /*    */ KC_Y, KC_U, KC_I, KC_O, KC_P,                  //
+        KC_A, KC_S, KC_D, KC_F, KC_G, /*    */ KC_H, KC_J, KC_K, KC_L, KC_QUOT,               //
+        _S(KC_Z), KC_X, KC_C, KC_V, KC_B, /*    */ KC_N, KC_M, KC_COMMA, KC_DOT, _S(KC_SLSH), //
+        KC_LCTL, KC_LGUI, RAISE, /*    */ LOWER, KC_LALT, FN                                  //
+        ),                                                                                    //
 
-    [_LOWER] = LAYOUT(                                                               //
-        KC_1, KC_2, KC_3, KC_4, KC_5, /* */ KC_MINS, KC_EQL, KC_DEL, KC_UP, KC_BSPC, //
-        KC_6, KC_7, KC_8, KC_9, KC_0, /* */ KC_BSLS, KC_SCLN, KC_LFT, KC_DN, KC_RGT, //
-        ___, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, /* */ ___, ___, ___, ___, ___,      //
-        ___, ___, ___, /* */ ___, ___, ___                                           //
-        ),                                                                           //
+    [_LOWER] = LAYOUT(                                                                  //
+        KC_1, KC_2, KC_3, KC_4, KC_5, /*    */ KC_MINS, KC_EQL, KC_DEL, KC_UP, KC_BSPC, //
+        KC_6, KC_7, KC_8, KC_9, KC_0, /*    */ KC_BSLS, KC_SCLN, KC_LFT, KC_DN, KC_RGT, //
+        ___, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, /*    */ ___, ___, ___, ___, ___,      //
+        ___, ___, ___, /* */ ___, ___, ___                                              //
+        ),                                                                              //
 
-    [_RAISE] = LAYOUT(                                                                         //
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, /* */ KC_UNDS, KC_PLUS, ___, ___, KC_TILDE,  //
-        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, /* */ KC_PIPE, KC_COLN, ___, ___, KC_GRV, //
-        ___, ___, ___, ___, ___, /* */ KC_LCBR, KC_RCBR, ___, ___, ___,                        //
-        ___, ___, ___, /* */ ___, ___, ___                                                     //
-        ),                                                                                     //
+    [_RAISE] = LAYOUT(                                                                            //
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, /*    */ KC_UNDS, KC_PLUS, ___, ___, KC_TILDE,  //
+        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, /*    */ KC_PIPE, KC_COLN, ___, ___, KC_GRV, //
+        ___, ___, ___, ___, ___, /*    */ KC_LCBR, KC_RCBR, ___, ___, ___,                        //
+        ___, ___, ___, /*    */ ___, ___, ___                                                     //
+        ),                                                                                        //
 
-    [_FN] = LAYOUT(                                                                          //
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, /* */ KC_PSCR, KC_SCRL, KC_PAUS, KC_INS, KC_CAPS, //
-        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, /* */ KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,    //
-        KC_MUTE, KC_VOLD, KC_VOLU, DM_REC1, DM_PLY1, /* */ ___, ___, ___, ___, ___,                  //
-        ___, ___, ___, /* */ ___, ___, ___                                                   //
-        ),                                                                                   //
+    [_FN] = LAYOUT(                                                                             //
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, /*    */ KC_PSCR, KC_SCRL, KC_PAUS, KC_INS, KC_CAPS, //
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, /*    */ KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,    //
+        KC_MUTE, KC_VOLD, KC_VOLU, ___, ___, /*    */ ___, ___, ___, ___, ___,                  //
+        ___, ___, ___, /*    */ ___, ___, ___                                                   //
+        ),                                                                                      //
 
-    [_BOTH] = LAYOUT(                                                      //
-        KC_ESC, ___, ___, ___, ___, /* */ ___, ___, ___, KC_PGUP, KC_BSPC, //
-        ___, ___, ___, ___, ___, /* */ ___, ___, KC_HOME, KC_PGDN, KC_END, //
-        ___, ___, ___, ___, ___, /* */ ___, ___, ___, ___, ___,            //
-        ___, ___, ___, /* */ ___, ___, ___                                 //
-        ),                                                                 //
+    [_BOTH] = LAYOUT(                                                          //
+        KC_ESC, ___, ___, ___, ___, /*    */ ___, ___, ___, KC_PGUP, KC_BSPC,  //
+        ___, ___, ___, ___, ___, /*    */ ___, ___, KC_HOME, KC_PGDN, KC_END,  //
+        ___, KC_CUT, KC_COPY, KC_PASTE, ___, /*    */ ___, ___, ___, ___, ___, //
+        ___, ___, ___, /*    */ ___, ___, ___                                  //
+        ),                                                                     //
 };
 
 // COMBO is everything !
-const uint16_t PROGMEM combo_esc[] = { KC_Q, KC_W, COMBO_END };
+const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM combo_bsp[] = {KC_O, KC_P, COMBO_END};
-#ifdef HOME_ROW
-const uint16_t PROGMEM combo_tab[] = {HOME_S, HOME_D, COMBO_END};
-const uint16_t PROGMEM combo_ent[] = {HOME_L, HOME_QUOT, COMBO_END};
-#else
 const uint16_t PROGMEM combo_tab[] = {KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM combo_ent[] = {KC_L, KC_QUOT, COMBO_END};
-#endif
-const uint16_t PROGMEM combo_alt[] = { KC_DOT, KC_COMMA, COMBO_END };
-const uint16_t PROGMEM combo_rst[] = { KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, COMBO_END };
+const uint16_t PROGMEM combo_alt[] = {KC_DOT, KC_COMMA, COMBO_END};
+const uint16_t PROGMEM combo_rst[] = {KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, COMBO_END};
 //
 combo_t key_combos[] = {
     COMBO(combo_bsp, KC_BSPC), //
@@ -103,8 +76,7 @@ combo_t key_combos[] = {
     COMBO(combo_rst, QK_BOOT)  //
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t* record)
-{
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     update_tri_layer(_LOWER, _RAISE, _BOTH);
     return true;
 }
